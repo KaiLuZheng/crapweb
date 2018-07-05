618adcfdd0dd486027c1a7ff06695f709ca04d8b
KailuZheng <lumen_o@126.com>
2018-07-05 17:34:56 +0800

    ex ok. next to do the headless

--- exhtml.py
+++ exhtml.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from bs4 import BeautifulSoup as bs
-
+import re
 
 with open('temp.html', 'r') as f:
     html = f.read()
@@ -21,8 +21,34 @@ with open('aim_text.html', 'w') as f:
 
 another = str(aim[0].find_all('div', class_='article_text')[0])
 
-with open('simple.html', 'w') as f:
-    f.write(another)
+title = re.findall(r'<h1>(.*?)</h1>', aim_text)
+print(title[0])
+
+title = re.findall(r'<p>(.*?)</p>', aim_text, re.S)
+
+for i in title:
+    t = i.replace('\n' or '\r' or '\\s' or '\r\n' or '\n\r', '')
+    if len(t) >= 1:
+        #print('%s'%t)
+        pass
+
+author = re.findall(r'<p.*?><span>(.*?)</span></p>', aim_text)[0]
+print(author)
+
+print(aim[0].find_all('p', class_='article_author'))
+
+
+
+'''
+for i in title:
+    if i == '\n' or i == '\r' or i == '\r\n' or i == '\n\r':
+        pass
+    else:
+        print('[%s]'%i)
+'''
+
+#with open('simple.html', 'w') as f:
+#    f.write(another)
 
 
 
